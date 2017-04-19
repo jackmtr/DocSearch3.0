b7c2785c5cf3c4a934b30d20d45f487e8af5a993
jackmtr <jackmtr@hotmail.com>
2017-04-19 11:03:24 -0700

    Fixed bug with Effective Date and Issue Date having small display issues

--- DocSearch2.1/ViewModels/PublicVM.cs
+++ DocSearch2.1/ViewModels/PublicVM.cs
@@ -17,9 +17,9 @@ namespace DocSearch2._1.ViewModels
         [Display(Name = "Type of Document")]
         public string DocumentTypeName { get; set; } //tbl_DocumentType.Name
 
-        //[Display(Name = "Issue Date")]
+        [Display(Name = "Issue Date")]
         //[DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:MMM dd yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
         public Nullable<DateTime> IssueDate { get; set; } //tbl_Document.Issue_DT
 
         public string Description { get; set; } //tbl_Document.Description
@@ -30,8 +30,9 @@ namespace DocSearch2._1.ViewModels
         public string CategoryName { get; set; } //tbl_Category.Name
 
         [Display(Name = "Effective Date")]
-        [DataType(DataType.Date)]
+        //[DataType(DataType.Date)]
         //[DisplayFormat(DataFormatString = "{0:d}")]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
         public Nullable<DateTime> EffectiveDate { get; set; } //tbl_DocReference.Date1_DT
 
         public string RefNumber { get; set; } //tbl_DocReference.RefNumber
