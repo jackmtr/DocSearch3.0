ee219820ffde5d7199250523de3aa6d2ad420592
jackmtr <jackmtr@hotmail.com>
2017-05-23 15:22:33 -0700

    mistakenly removed the front page controller

---
+++ DocSearch2.1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocSearch2._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //The front page to input the folder info to look up, this page isnt needed in production as WEIS should be supplying the info
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
